4f4805582222ddcebbbaf3fc6cc6a0ebc67b5b76
LSH <koelkorea@gmail.com>
2024-01-10 04:42:26 +0900

    1/10 commit
    ㅇㅇ

--- server.js
+++ server.js
@@ -7,8 +7,18 @@ const express = require('express');
 // express 기본 생성자의 결과 객체를 받는 변수 (해당 객체가 추후 API를 포함한 모든 메서드를 담당)
 const app = express();
 
+// express().set('view engine', '템플릿 엔진명');
+//  : express 라이브러리 환경에서 '템플릿 엔진'을 쓸 수 있게 하는 코드 (ejs를 써보도록 하겠음)
+app.set('view engine', 'ejs');
+
+// express().use(express.static(__dirname, '목표 static 폴더 경로(보통은 /public )'));  
+//  : express.static는 특정 폴더안의 파일들을 static 파일로 지정할 수 있으며, use()메서드를 통해 express 객체가 해당 폴더의 내용을 쓸 수 있도록 등록하는 메서드
+app.use(express.static(__dirname + '/public'));     // 이를 통해 html파일에 css파일을 <link>태그로 붙여줄 수 있음
+
 // MongoDB 모듈 import에 해당하는 내용
-const { MongoClient } = require('mongodb')
+//  - MongoClient : DB접속시 생성된 해당 유저정보를 담는 JS객체에 해당
+//  - ObjectId : 사용자가 DB에서 ID기반 검색시 생성된 JS에서 이 ID정보를 인식하고 DB에 검색 명령 내릴때 전달가능하게 하는 JS객체에 해당
+const { MongoClient, ObjectId } = require('mongodb')
 
 // MongoDB 서버 접속 결과 및 DB제어 함수를 사용하기 위한 변수
 let db;
@@ -61,17 +71,118 @@ app.get('/', (요청, 응답)=>{
     응답.sendFile(__dirname + '/index.html');         // (중요) __dirname : 현재 server.js가 위치한 절대경로를 담는 일종의 static 변수
 }) 
 
-app.get('/list', async  (요청, 응답) =>{
+// MongoDB를 통해 받은 데이터를 ejs 템플릿 엔진을 통해 받고 난 뒤 SSR 방식으로 웹페이지 동적 제작
+
+// 도메인/list를 들어가게 된다면?
+//  -> 1. (비동기 코드 명심!) MongoDB에서 post라는 collenction의 데이터를 읽고, 
+//     2. 그걸 list.ejs라는 템플릿 엔진 view 파일에 특정한 변수명으로 대입해서 보내준 뒤, 
+//     3. 이를 웹페이지로 렌더링해서 그 결과물을 사용자에게 보내라
+app.get('/list', async (요청, 응답) =>{
+
+    // (중요!) MongoDB의 데이터를 읽는 API는 비동기 코드임을 명심 = then() catch() finally()나 async, await을 써서 제어해야함
+
+    // client.db('forum').collection('post').find().toArray();
+    //  : MongoDB의 forum이라는 프로젝트의 post라는 컬렉션의 복수 데이터를 객체배열 형태로 가져오는 메서드
     let result = await db.collection('post').find().toArray();
-    응답.render('list.ejs', { 글목록 : result });
+
+    // 날짜를 보내주기 위한 목적의 변수
+    let date = new Date();
+
+    console.log(result);
+
+    // list.ejs라는 템플릿 파일에 result, date에 담겨있던 자료값이 글목록이라는 변수명으로 옮겨짐
+    응답.render('list.ejs', { 글목록 : result, 날짜 : date });       
 })
 
-// /time 이라고 접속하면 현재 서버의 시간을 보내주는 기능을 만들어봅시다.
+// (과제) /time 이라고 접속하면 현재 서버의 시간을 보내주는 기능을 만들어봅시다.
+app.get('/time', async (요청, 응답) =>{
 
-// ejs로 웹페이지를 만들어서 거기 안에 서버의 시간을 박아넣어서 보내주면 될 것 같군요. 
+    // (팁) 서버의 시간은 server.js 파일 아무데서나 new Date() 라고 쓰면 나옵니다
+    let result = new Date();
+    응답.send(result);
+})
 
-// (팁) 서버의 시간은 server.js 파일 아무데서나 new Date() 라고 쓰면 나옵니다
+// (중요) 템플릿 엔진으로부터 API요청이 들어왔을 경우, 그 요청(request)의 body 영역을 파싱(parse)하기 위해 사용되는 미들웨어에 대한 보일러 플레이트 코드를 입력해야 함
+//   -> 이를 사용하지 않을 경우, 요청이 들어온 데이터의 body영역이 인간이 알기 어려운 용어로 전달되거나 예기치 못한 에러가 발생할 수 있음
 
-// express().use(express.static(__dirname, '목표 static 폴더 경로(보통은 /public )'));  
-//  : express.static는 특정 폴더안의 파일들을 static 파일로 지정할 수 있으며, use()메서드를 통해 express 객체가 해당 폴더의 내용을 쓸 수 있도록 등록하는 메서드
-app.use(express.static(__dirname + '/public'));     // 이를 통해 html파일에 css파일을 <link>태그로 붙여줄 수 있음
+// app.use(express.json())
+//  : express 라이브러리 환경에서 JSON 형태의 들어온 요청(request)의 body 영역을 파싱(parse)하기 위해 사용되는 미들웨어(middleware)
+
+app.use(express.json());
+
+// app.use(express.urlencoded())
+//  : express 라이브러리 환경에서 JSON 형태가 아닌 form 데이터나 멀티파트 등 다른 형태로 요청이 들어온 요청(request)의 body 영역을 파싱(parse)하기 위해 사용되는 URL 인코딩 관련 미들웨어(middleware)
+
+//  app.use(express.urlencoded( {extended:boolean} ) )
+//   : urlencoded 미들웨어에서 전달된 url 쿼리 스트링을 보안 이슈가 있는 중첩 객체를 통해 파싱할건지를 결정하는 설정값
+//     (= 결과적으로 true, false에 따라 파싱에 사용하는 query String 라이브러리가 달라짐)
+//         -> true (기본)  : 객체 형태로 전달된 데이터 내에서 또 다른 중첩된 객체를 허용함 (= body-parser 모듈을 사용가능하게 하는 초석 )
+//                            -> url 쿼리 스트링을 파싱하는데, 따로 설치가 필요하지만 추가적인 보안 확장이 가능한 npm qs 라이브러리 사용함
+//            false        : 객체 형태로 전달된 데이터 내에서 또 다른 중첩된 객체를 허용 X
+//                            -> url 쿼리 스트링을 파싱하는데, node.js에 기본으로 내장된 queryString 라이브러리 사용함
+app.use(express.urlencoded({extended:true})) ;
+
+// (과제) 도메인/add로 POST요청하면, 그 제목과 내용을 DB에 저장하기
+
+// 도메인/write라는 url에 GET 형식의 요청이 들어오면 입력페이지 보여주는 API
+app.get('/write', (요청, 응답)=>{
+    응답.render('write.ejs');
+}); 
+
+// 입력페이지인 write.ejs 템플릿에서 도메인/add라는 url에 POST 형식으로 보낸 form 데이터를 서버에서 받아 DB에 입력하는 API 구현
+app.post('/add', async (요청, 응답)=>{
+    // 상단의 보일러 플레이트 코드를 입력했기에, 사용자가 form 요청으로 보낸 input 데이터를 '요청parameter.body' 한 방으로 바로 JSON으로 파싱된 형식으로 볼 수 있음
+    console.log(요청.body);
+    
+    // 서버 API를 거치게 될 때, validator 처리를 위한 조건문
+    if (요청.body.title == '') {
+
+        응답.send('제목을 적어주시길..')
+
+    } else {
+
+        // 오류처리 방지용 try, catch 구문
+        try{
+            // client.db('forum').collection('post').insertOne({title : 요청.body.title, content : 요청.body.content});
+            //  : MongoDB의 forum이라는 프로젝트의 post라는 컬렉션에 js객체 형식으로 적힌 {}안의 데이터를 기입
+            await db.collection('post').insertOne({title : 요청.body.title, content : 요청.body.content});
+
+            // 응답parameter명.redirect('/list');
+            //  : 도메인/list url의 API로 강제로 보내기
+            응답.redirect('/list');
+        } catch (e) {
+            console.log(e);
+            응답.status(500).send('DB에러남');
+        }
+    } 
+
+});
+
+// (과제) 사용자가 목록 화면에서 도메인/detail/:parameter로 GET방식의 API를 요청하면, detail.ejs 템플릿을 따르는 상세페이지 보여주기
+app.get('/detail/:id', async (요청, 응답) => {
+
+    // 오류처리 방지용 try, catch 구문
+    try{
+
+        // url 파라미터 문법을 사용한다면, '요청parameter'.params를 통해 url에 전해진 url파라미터 내용을 js객체 형식으로 한번에 보는게 가능함
+        console.log(요청.params);
+        
+        // client.db('forum').collection('post').findOne({_id : new ObjectId(요청.params.id)});
+        //  : MongoDB의 forum이라는 프로젝트의 post라는 컬렉션에 _id 내용이 url에 적혀있는것과 일치하는 녀석을 가져올것
+        //     -> (주의) _id를 검색대상으로 쓰려면
+        //          1. new ObjectId(id번호) 형식으로 내용을 기입
+        //          2. const { ObjectId } = require('mongodb') 코드가 기입되어 ObjectId 객체를 쓸수 있어야 함
+        let result = await db.collection('post').findOne({_id : new ObjectId(요청.params.id)});
+        console.log(result);
+
+        if (result == null) {
+            응답.status(400).send('그런 글 없음')
+        } else {
+            응답.render('detail.ejs', { 상세글 : result })
+        }
+
+    } catch (e) {
+        console.log(e);
+        응답.status(500).send('DB에러남... id에 이상한걸 넣는군..');
+    }
+});
